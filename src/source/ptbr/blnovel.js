4fcda5433b99a99279c9dd3dc658586cfddae8ed
Harry <30308156+harryChrist@users.noreply.github.com>
2024-08-21 22:34:55 -0300

    quase finalizada

--- src/source/ptbr/blnovel.js
+++ src/source/ptbr/blnovel.js
@@ -28,10 +28,13 @@ class BlNovelsProvider extends BaseProvider {
             const a = $(tab).find('.post-title h3 a');
             const latest = $(tab).find('.latest-chap .chapter a').text();
             const votes = $(tab).find('.rating .total_votes').text();
+            const imageURL = $(tab).find('.tab-thumb a img').attr('src');
             results.push({
-                title: a.text().trim(),
                 url: this.getFullUrl(a.attr('href')),
-                info: `${latest} | Rating: ${votes}`
+                title: a.text().trim(),
+                imageURL: imageURL,
+                chapter: latest,
+                ranking: votes
             });
         });
 
@@ -39,47 +42,62 @@ class BlNovelsProvider extends BaseProvider {
     }
 
     async readNovelInfo(novelUrl) {
-        this.novelUrl = novelUrl;
-        console.debug("Visiting", this.novelUrl);
-        const { data } = await axios.get(this.novelUrl);
-        const $ = cheerio.load(data);
+        try {
+            this.novelUrl = novelUrl;
+            console.debug("Visiting", this.novelUrl);
+            const { data } = await axios.get(this.novelUrl);
+            const $ = cheerio.load(data);
 
-        const possibleTitle = $('.post-title h1');
-        possibleTitle.find('span').remove();
-        this.novelTitle = possibleTitle.text().trim();
-        console.info("Novel title:", this.novelTitle);
+            const possibleTitle = $('.post-title h1');
+            possibleTitle.find('span').remove();
+            this.novelTitle = possibleTitle.text().trim();
+            console.info("Novel title:", this.novelTitle);
 
-        const possibleImage = $('.summary_image a img');
-        this.novelCover = possibleImage.attr('src') ? this.getFullUrl(possibleImage.attr('src')) : '';
-        console.info("Novel cover:", this.novelCover);
+            const possibleImage = $('.summary_image a img');
+            this.novelCover = possibleImage.attr('src') ? this.getFullUrl(possibleImage.attr('src')) : '';
+            console.info("Novel cover:", this.novelCover);
 
-        this.novelAuthor = $('.author-content a[href*="novel-author"]')
-            .map((i, el) => $(el).text().trim())
-            .get()
-            .join(' ');
-        console.info("Novel author:", this.novelAuthor);
+            this.novelAuthor = $('.author-content a[href*="autor"]')
+                .map((i, el) => $(el).text().trim())
+                .get()
+                .join(' ');
+            console.info("Novel author:", this.novelAuthor);
 
-        const chapterListUrl = this.getFullUrl('ajax/chapters');
-        const { data: chapterData } = await axios.post(chapterListUrl, null, { headers: { accept: '*/*' } });
-        const chapterSoup = cheerio.load(chapterData);
-
-        this.chapters = [];
-        this.volumes = [];
-
-        chapterSoup('.wp-manga-chapter a[href*="/novel"]').each((i, a) => {
-            const title = $(a).text().replace(/^\d+\s*-\s*/, '').trim();
-            const chapId = this.chapters.length + 1;
-            const volId = 1 + Math.floor(this.chapters.length / 100);
-            if (chapId % 100 === 1) {
-                this.volumes.push({ id: volId });
-            }
-            this.chapters.push({
-                id: chapId,
-                volume: volId,
-                title: title,
-                url: this.getFullUrl($(a).attr('href')),
+            const chapterListUrl = this.getFullUrl(this.novelUrl+ 'ajax/chapters');
+            const { data: chapterData } = await axios.post(chapterListUrl, null, {
+                headers: {
+                    'accept': '*/*',
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+                    'Referer': this.novelUrl, // Referer para indicar de onde a requisição está sendo feita
+                    'X-Requested-With': 'XMLHttpRequest' // Indica que a requisição foi feita via AJAX
+                }
             });
-        });
+            const chapterSoup = cheerio.load(chapterData);
+
+            this.chapters = [];
+            this.volumes = [];
+
+
+            chapterSoup('.wp-manga-chapter a[href*="/novel"]').each((i, a) => {
+                const title = $(a).text().replace(/^\d+\s*-\s*/, '').trim();
+                const chapId = this.chapters.length + 1;
+                const volId = 1 + Math.floor(this.chapters.length / 100);
+                if (chapId % 100 === 1) {
+                    this.volumes.push({ id: volId });
+                }
+                this.chapters.push({
+                    id: chapId,
+                    volume: volId,
+                    title: title,
+                    url: this.getFullUrl($(a).attr('href')),
+                });
+            });
+
+            return this.chapters
+        } catch (error) {
+            console.error("Erro ao buscar o conteúdo:", error.message);
+            throw error;
+        }
     }
 
     async downloadChapterBody(chapter) {
